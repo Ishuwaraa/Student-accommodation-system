212fea1cc8953defabbe18252fc2e1473ba452b7
janaaaac <cjdissanayake@students.nsbm.ac.lk>
2024-03-04 18:52:03 +0530

    Update home.php

--- src/php/home.php
+++ src/php/home.php
@@ -1,6 +1,11 @@
-<?php 
-include_once 'src/view/header.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
 
 <main>
     <article class="article">
@@ -456,8 +461,8 @@ include_once 'src/view/header.php';
 
     </article>
   </main>
+  
+</body>
+</html>
 
-  <?php 
-include_once 'src/view/footer.php';
-?>
 
